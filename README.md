05f3e766aefdc833cbcf0de827ee0571e1d1b121
Muthyala <2159799@cognizant.com>
2024-07-11 18:43:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# fundamentals_part_1" 
---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+function logger(){
+    console.log("my name is Ranjith")
+}
+logger();
+
+
+function fruitmixer(apples,oranges){
+    console.log(apples,oranges);
+    const juice = (`juice with ${apples}apples and ${oranges}oranges`);
+    return juice;
+}
+ 
+const applesjuice = fruitmixer(0,5);
+console.log(applesjuice);
+
+function ranage1(birthyear){
+    return 2000 - birthyear;
+}
+const age1=ranage1(1999);
+console.log(age1);
+
+const ranage2 = function (birthyear){
+    return 2000 - birthyear
+}
+const age2 = ranage2(1999);
+console.log(age1,age2);
+*/
+function iphone(actualprice){
+    return 2000 - actualprice;
+}
+const price = iphone(1000);
+console.log(price);
+
+const age3 = birthyear => 2037 - birthyear;
+const exage = age3(2000);
+console.log(exage);
